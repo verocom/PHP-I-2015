ac909a0d65bece9cb0b45517a1f0b0052b4a46bc
Carl-Hugo Marcotte <carlhugo@gmail.com>
2015-01-28 15:56:50 -0500

    Mise à jour du format du code d'un fichier.

--- Cours04/Exercices/BoucleWhileTableau.php
+++ Cours04/Exercices/BoucleWhileTableau.php
@@ -1,42 +1,38 @@
 <!doctype html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>PHP - Boucles</title>
-</head>
-<body>
-	<table border="1">
-		<tr>
-			<th>Index</th>
-			<th>Index mod 2</th>
-			<th>Ligne Pair ?</th>
-		</tr>
-		<?php
-		$index = 0;
-		while ($index < 20)
-		{
-			?>
-			<tr>
-				<td><?= $index ?></td>
-				<td><?= $index % 2 ?></td>
-				<td align="center">
-					<?php
-					if ($index % 2 == 0)
-					{
-						echo("Oui");
-					}
-					else
-					{
-						echo("Non");
-					}
-					?>
-				</td>
-			</tr>
-		    <?php
-			$index = $index + 1;
-		}
-		?>
-	</table>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <title>PHP - Boucles</title>
+    </head>
+    <body>
+        <table border="1">
+            <tr>
+                <th>Index</th>
+                <th>Index mod 2</th>
+                <th>Ligne Pair ?</th>
+            </tr>
+            <?php
+            $index = 0;
+            while ($index < 20) {
+                ?>
+                <tr>
+                    <td><?= $index ?></td>
+                    <td><?= $index % 2 ?></td>
+                    <td align="center">
+                        <?php
+                        if ($index % 2 == 0) {
+                            echo("Oui");
+                        } else {
+                            echo("Non");
+                        }
+                        ?>
+                    </td>
+                </tr>
+                        <?php
+                        $index = $index + 1;
+                    }
+                    ?>
+        </table>
+    </body>
 </html>
 
